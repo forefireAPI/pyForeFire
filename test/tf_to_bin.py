8cc5d18db513dc9019843d4c479ef16d36abcd23
Thoreau Romain <romain.thoreau@cnes.fr>
2024-09-05 16:55:32 +0200

    add RothAndrews fuel table

--- test/tf_to_bin.py
+++ test/tf_to_bin.py
@@ -14,15 +14,15 @@ if not os.path.exists(bin_model_path):
         model, 
         bin_model_path, 
         input_names=[
-            "normalWind", 
-            "slope", 
-            "fuel.Md", 
-            "fuel.DeltaH", 
-            "fuel.DeltaH", 
-            "fuel.DeltaH",
-            "fuel.DeltaH",
-            "fuel.DeltaH",
-            "fuel.DeltaH"
+            "normalWind", # Normal wind
+            "slope", # Slope
+            "fuel.Md", # Fuel particle moisture content
+            "fuel.DeltaH", # Fuel particle low heat content
+            "fuel.sd", # Fuel Particle surface area to volume ratio (1/ft)
+            "fuel.e", # Fuel depth (ft)
+            "fuel.Rhod", # Ovendry particle density
+            "fuel.me", # Moisture content of extinction
+            "fuel.Sigmad" # Ovendry fuel loading 
             ], 
         output_names=['ROSx']
         )
