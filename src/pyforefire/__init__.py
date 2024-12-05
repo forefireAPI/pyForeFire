3cabf10b68be0dc50f710f2078f535ad2a715ecb
FILIPPI Jean-Baptiste <filippi@univ-corse.fr>
2024-12-05 19:29:31 +0100

    removed rogue result files

--- src/pyforefire/__init__.py
+++ src/pyforefire/__init__.py
@@ -1,7 +1,6 @@
 # src/pyforefire/__init__.py
 
 from .helpers import *
-from .tf_helpers import *
 from ._pyforefire import *  # Import the C++ extension
 
-__all__ = ['helpers', 'tf_helpers', '_pyforefire']
+__all__ = ['helpers', '_pyforefire']
